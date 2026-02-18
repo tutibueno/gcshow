5c842594498f6a5f4fe4946e110119ee7cd938f5
Tuti Bueno <tuti.bueno@gmail.com>
2026-02-18 13:16:30 -0300

    Correção titulo

--- app/Views/home.php
+++ app/Views/home.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Games Collection Show</title>
+    <title>Game Collection Show</title>
     <link rel="icon" type="image/x-icon" href="<?= base_url('public/favicon.ico') ?>">
     <link rel="shortcut icon" href="<?= base_url('public/favicon.ico') ?>">
     <link rel="apple-touch-icon" sizes="180x180" href="<?= base_url('public/apple-touch-icon.png') ?>">
